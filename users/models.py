606e26b072887308c11b0c56f03ab29c7a2ba3b6
ProgF <alimbayevaf@gmail.com>
2019-12-02 17:36:09 +0600

    Functionated user

--- users/models.py
+++ users/models.py
@@ -35,7 +35,12 @@ class Profile(models.Model):
                                                  allowed_extensions=[
                                                      'png',
                                                      'jpg', 'jpeg'])])
-    rating = models.IntegerField(default=0  )
+    rating = models.IntegerField(default=0)
     post_rating = models.IntegerField(default=0)
     comment_rating = models.IntegerField(default=0)
     comment_sorting = models.IntegerField(choices=COMMENT_SORT_TYPES, default=BY_RATING)
+
+
+class UserRelation(models.Model):
+    subscriber = models.ForeignKey(MainUser, on_delete=models.CASCADE, related_name='my_subscriptions')
+    subscribed_to = models.ForeignKey(MainUser, on_delete=models.CASCADE, related_name='my_subscribers')
--- users/urls.py
+++ users/urls.py
@@ -1,18 +1,16 @@
 from django.urls import path
-from users.views import RegisterViewSet, ProfileViewSet, UserViewSet
+from users.views import RegisterViewSet, ProfileViewSet, UserViewSet,subscription_view
 from rest_framework import routers
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('login/', obtain_jwt_token),
-    path('profile/', ProfileViewSet.as_view({
-        'get': 'retrieve',
-        'put': 'update'}), name='profile-detail'),
-
     path('delete/', UserViewSet.as_view({
         'delete': 'destroy'}), name='delete-user'),
+    path('subscribe_to/<int:pk>/', subscription_view)
 ]
 
 router = routers.DefaultRouter()
 router.register('register', RegisterViewSet, base_name='users')
+router.register('profile', ProfileViewSet, base_name='users')
 urlpatterns += router.urls
