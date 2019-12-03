d4fafdcde269c97178f9d5585cc0e42d42058c17
Aiganym <azhandaulet@one.kz>
2019-12-03 16:18:39 +0600

    add community posts

--- community/urls.py
+++ community/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from community.views import CommunityAPIView, CommunityDetailAPIView, subscription_view
+from community.views import CommunityAPIView, CommunityDetailAPIView, subscription_view, community_posts
 from rest_framework import routers
 
 urlpatterns = [
     path('communities/', CommunityAPIView.as_view()),
     path('communities/<int:pk>/', CommunityDetailAPIView.as_view()),
-    # path('communities/<int:pk>/posts', ),
+    path('communities/<int:pk>/posts', community_posts),
     path('subscribe_to/<int:pk>/', subscription_view)
 ]
 
